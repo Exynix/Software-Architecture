1ad6dd1e4ef233dc44b4b32628564e58888a671f
David <davids_barretom@javeriana.edu.co>
2024-05-25 16:48:21 -0500

    Addition of checkout  files

--- MiPortal/frontend/src/routes.jsx
+++ MiPortal/frontend/src/routes.jsx
@@ -1,10 +1,12 @@
 import React from "react"
 import { Routes, Route} from "react-router-dom";
 import LoginPage from "./authentication/LoginPage.jsx";
+import Checkout from "./payments/Checkout.js";
 
 const AppRoutes = () => (
     <Routes>
         <Route path = "/login" element={<LoginPage />} />
+        <Route path = "/checkout" element={<Checkout />} />
     </Routes>
 );
 
