4f8e0c383adc8fd6ce99c032725d2cf6de76f9c8
David <davids_barretom@javeriana.edu.co>
2024-05-26 12:39:01 -0500

    Traduction of authentication pages

--- MiPortal/frontend/src/authentication/LoginPage.jsx
+++ MiPortal/frontend/src/authentication/LoginPage.jsx
@@ -18,15 +18,15 @@ function LoginPage() {
            <div className="flex align-items-center justify-content-center">
                <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
                    <div className="text-center mb-5">
-                       <img src="/demo/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3"/>
-                       <div className="text-900 text-3xl font-medium mb-3">Welcome Back</div>
-                       <span className="text-600 font-medium line-height-3">Don't have an account?</span>
-                       <Link to={"/signup"} className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Create today!</Link>
+                       <img src="https://cdn-icons-png.flaticon.com/128/7062/7062990.png" alt="hyper" height={50} className="mb-3"/>
+                       <div className="text-900 text-3xl font-medium mb-3">Bienvenido!</div>
+                       <span className="text-600 font-medium line-height-3">Aún no tienes cuenta?</span>
+                       <Link to={"/signup"} className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Unete ahora!</Link>
                    </div>
 
                    <div>
-                       <label htmlFor="email" className="block text-900 font-medium mb-2">Username</label>
-                       <InputText id="email" type="text" placeholder="Username" className="w-full mb-3"/>
+                       <label htmlFor="email" className="block text-900 font-medium mb-2">Usuario</label>
+                       <InputText id="email" type="text" placeholder="Usuario" className="w-full mb-3"/>
 
                        <div className="flex align-items-center justify-content-between mb-6">
                            <div className="flex align-items-center">
@@ -34,8 +34,8 @@ function LoginPage() {
                                          className="mr-2"/>
                                <label htmlFor="rememberme">Remember me</label>
                            </div>
-                           <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Forgot
-                               your username?</a>
+                           <a className="font-medium no-underline ml-2 text-blue-500 text-right cursor-pointer">Olvidaste
+                               tu usuario?</a>
                        </div>
 
                        <Button label="Sign In" icon="pi pi-user" className="w-full"/>
--- MiPortal/frontend/src/authentication/SignupPage.jsx
+++ MiPortal/frontend/src/authentication/SignupPage.jsx
@@ -18,21 +18,21 @@ function SignupPage() {
            <div className="flex align-items-center justify-content-center">
                <div className="surface-card p-4 shadow-2 border-round w-full lg:w-6">
                    <div className="text-center mb-5">
-                       <img src="/demo/images/blocks/logos/hyper.svg" alt="hyper" height={50} className="mb-3"/>
-                       <div className="text-900 text-3xl font-medium mb-3">Create your account!</div>
-                       <span className="text-600 font-medium line-height-3">Already have an account?</span>
+                       <img src="https://cdn-icons-png.flaticon.com/128/7062/7062990.png" alt="hyper" height={50} className="mb-3"/>
+                       <div className="text-900 text-3xl font-medium mb-3">Crea tu cuenta!</div>
+                       <span className="text-600 font-medium line-height-3">Ya eres usuario?</span>
                        <Link to={"/login"} className="font-medium no-underline ml-2 text-blue-500 cursor-pointer">Log in!</Link>
                    </div>
 
                    <div>
-                       <label htmlFor="email" className="block text-900 font-medium mb-2">Username</label>
-                       <InputText id="email" type="text" placeholder="Username" className="w-full mb-3"/>
+                       <label htmlFor="email" className="block text-900 font-medium mb-2">Usuario</label>
+                       <InputText id="email" type="text" placeholder="Usuario" className="w-full mb-3"/>
 
                        <div className="flex align-items-center justify-content-between mb-6">
                            <div className="flex align-items-center">
                                <Checkbox id="rememberme" onChange={e => setChecked(e.checked)} checked={checked}
                                          className="mr-2"/>
-                               <label htmlFor="rememberme">Remember me</label>
+                               <label htmlFor="rememberme">Recuerdame</label>
                            </div>
                        </div>
 
