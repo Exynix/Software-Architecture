f26af7fc431953d08b92779777d795e9dcf492b8
David <davids_barretom@javeriana.edu.co>
2024-05-26 10:25:34 -0500

    Changes to login page to only have a field for username.

--- MiPortal/frontend/src/authentication/LoginPage.jsx
+++ MiPortal/frontend/src/authentication/LoginPage.jsx
@@ -24,11 +24,8 @@ function LoginPage() {
                    </div>
 
                    <div>
-                       <label htmlFor="email" className="block text-900 font-medium mb-2">Email</label>
-                       <InputText id="email" type="text" placeholder="Email address" className="w-full mb-3"/>
-
-                       <label htmlFor="password" className="block text-900 font-medium mb-2">Password</label>
-                       <InputText id="password" type="password" placeholder="Password" className="w-full mb-3"/>
+                       <label htmlFor="email" className="block text-900 font-medium mb-2">Username</label>
+                       <InputText id="email" type="text" placeholder="Username" className="w-full mb-3"/>
 
                        <div className="flex align-items-center justify-content-between mb-6">
                            <div className="flex align-items-center">
